b31b59a10d25eba74df4d7a4f020a8340ea9f91e
Danil Zasypkin <danil@littleinferno.space>
2023-11-09 11:58:52 +0500

    kek

--- index.js
+++ index.js
@@ -7,69 +7,75 @@ const main = async () => {
 
   const {payload} = github.context;
 
-  core.info(`${JSON.stringify(payload)}`);
+  const {INVITE_TOKEN} = process.env;
 
-  try {
-    const {INVITE_TOKEN} = process.env;
+  const octokit = github.getOctokit(INVITE_TOKEN);
 
-    const octokit = github.getOctokit(INVITE_TOKEN);
+  octokit.rest.projects.removeCollaborator({
+    owner: 'little-inferno',
+    repo: 'exercises-test',
+    username: 'students'
+  })
 
-    const org = core.getInput('organization', {required: true});
+  // try {
 
-    const [username, group] = payload.issue.body.split("|")
-
-    const role = payload.issue.labels[0].name
-
-    try {
-      await octokit.orgs.checkMembership({
-        org,
-        username: payload.issue.user.login,
-      });
-    } catch (error) {
-
-      await octokit.rest.teams.addOrUpdateMembershipForUserInOrg({
-        org: org,
-        team_slug: group,
-        username: username,
-        role: 'member',
-      });
-
-      await octokit.rest.issues.createComment({
-        owner: payload.repository.owner.login,
-        repo: payload.repository.name,
-        issue_number: payload.issue.number,
-        body: "Access granted",
-      });
-
-      await octokit.rest.issues.update({
-        owner: payload.repository.owner.login,
-        repo: payload.repository.name,
-        issue_number: payload.issue.number,
-        state: 'closed',
-      });
-    }
-  } catch (error) {
-
-    if (error.message.toString().includes('already a part')) {
-
-      await octokit.rest.issues.createComment({
-        owner: payload.repository.owner.login,
-        repo: payload.repository.name,
-        issue_number: payload.issue.number,
-        body: "Access already granted",
-      });
-
-      await octokit.rest.issues.update({
-        owner: payload.repository.owner.login,
-        repo: payload.repository.name,
-        issue_number: payload.issue.number,
-        state: 'closed',
-      });
-
-    }
-
-    return core.setFailed(error.message);
-  }
+  //
+  //   const org = core.getInput('organization', {required: true});
+  //
+  //   const [username, group] = payload.issue.body.split("|")
+  //
+  //   const role = payload.issue.labels[0].name
+  //
+  //   try {
+  //     await octokit.orgs.checkMembership({
+  //       org,
+  //       username: payload.issue.user.login,
+  //     });
+  //   } catch (error) {
+  //
+  //     await octokit.rest.teams.addOrUpdateMembershipForUserInOrg({
+  //       org: org,
+  //       team_slug: group,
+  //       username: username,
+  //       role: 'member',
+  //     });
+  //
+  //     await octokit.rest.issues.createComment({
+  //       owner: payload.repository.owner.login,
+  //       repo: payload.repository.name,
+  //       issue_number: payload.issue.number,
+  //       body: "Access granted",
+  //     });
+  //
+  //     await octokit.rest.issues.update({
+  //       owner: payload.repository.owner.login,
+  //       repo: payload.repository.name,
+  //       issue_number: payload.issue.number,
+  //       state: 'closed',
+  //     });
+  //   }
+  // } catch (error) {
+  //
+  //   if (error.message.toString().includes('already a part')) {
+  //
+  //     await octokit.rest.issues.createComment({
+  //       owner: payload.repository.owner.login,
+  //       repo: payload.repository.name,
+  //       issue_number: payload.issue.number,
+  //       body: "Access already granted",
+  //     });
+  //
+  //     await octokit.rest.issues.update({
+  //       owner: payload.repository.owner.login,
+  //       repo: payload.repository.name,
+  //       issue_number: payload.issue.number,
+  //       state: 'closed',
+  //     });
+  //
+  //   }
+  //
+  //   return core.setFailed(error.message);
+  // }
   return core.setOutput('Invitation sent');
 };
 main();
